6ad7effaf6fb2a8027beb0f936179df17a733467
Chloe Prime <chloe_von_einzbern@qq.com>
2026-03-11 13:11:48 +0800

    Better Async API
    (cherry picked from commit 06454ae4f5f83021fde76a488e526876e88ed8ab)

--- src/main/java/mod/chloeprime/gunsmithlib/mixin/client/MixinGunAnimationStateContext.java
+++ src/main/java/mod/chloeprime/gunsmithlib/mixin/client/MixinGunAnimationStateContext.java
@@ -11,6 +11,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
+import net.neoforged.fml.LogicalSide;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -85,6 +86,11 @@ public abstract class MixinGunAnimationStateContext implements AbstractGunAnimat
     @Override
     public Optional<LivingEntity> gunsmithlib$getShooter() {
         return Optional.ofNullable(Minecraft.getInstance().player);
+    }
+
+    @Override
+    public LogicalSide gunsmithlib$getSide() {
+        return LogicalSide.CLIENT;
     }
 
     // 镜喵背包供弹实现
