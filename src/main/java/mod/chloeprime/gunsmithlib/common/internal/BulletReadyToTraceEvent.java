34a4947f46c959df1e84d1bab09ec8c5564f0d25
Chloe Prime <chloe_von_einzbern@qq.com>
2026-03-20 12:16:13 +0800

    增强高速智能子弹的命中率。现在，你无法逃脱。
    (cherry picked from commit a40db54af3d30bc7eab059bd7ed1f0bb94da5c3b)

--- src/main/java/mod/chloeprime/gunsmithlib/common/internal/BulletReadyToTraceEvent.java
+++ src/main/java/mod/chloeprime/gunsmithlib/common/internal/BulletReadyToTraceEvent.java
@@ -4,6 +4,7 @@ import mod.chloeprime.gunsmithlib.api.util.Rangefinder;
 import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
+import net.neoforged.fml.LogicalSide;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.entity.EntityEvent;
 import org.jetbrains.annotations.ApiStatus;
@@ -13,13 +14,19 @@ import javax.annotation.Nonnull;
 public class BulletReadyToTraceEvent extends EntityEvent {
     private final Projectile bullet;
     private final Vec3 start, end;
+    private final LogicalSide side;
 
     @ApiStatus.Internal
-    public BulletReadyToTraceEvent(Projectile bullet, Vec3 start, Vec3 end) {
+    public BulletReadyToTraceEvent(Projectile bullet, Vec3 start, Vec3 end, LogicalSide side) {
         super(bullet);
         this.bullet = bullet;
         this.start = start;
         this.end = end;
+        this.side = side;
+    }
+
+    public LogicalSide getSide() {
+        return side;
     }
 
     @Override
@@ -37,10 +44,6 @@ public class BulletReadyToTraceEvent extends EntityEvent {
 
     @ApiStatus.Internal
     public static void onBulletTick(Projectile bullet, int pierce) {
-        if (bullet.level().isClientSide()) {
-            return;
-        }
-
         var velocity = bullet.getDeltaMovement();
         if (velocity.lengthSqr() <= 1e-6) {
             return;
@@ -52,7 +55,8 @@ public class BulletReadyToTraceEvent extends EntityEvent {
         var end = start.add(estimated.asHitResult().getType() == HitResult.Type.MISS
                 ? velocity
                 : direction.scale(estimated.getLength()));
-        var event = new BulletReadyToTraceEvent(bullet, start, end);
+        var side = bullet.level().isClientSide() ? LogicalSide.CLIENT : LogicalSide.SERVER;
+        var event = new BulletReadyToTraceEvent(bullet, start, end, side);
         NeoForge.EVENT_BUS.post(event);
     }
 }
