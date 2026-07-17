35e1e8789e98b488e836dfc7116af2d80f36037c
Chloe Prime <chloe_von_einzbern@qq.com>
2026-07-17 22:02:04 +0800

    修复榴弹客户端弹道粒子位置不正确的 bug

--- src/main/java/mod/chloeprime/gunsmithlib/mixin/bugfix/tacz/TracerStartPosFixDisableBulletTrackingMixin.java
+++ src/main/java/mod/chloeprime/gunsmithlib/mixin/bugfix/tacz/TracerStartPosFixDisableBulletTrackingMixin.java
@@ -1,9 +1,10 @@
 package mod.chloeprime.gunsmithlib.mixin.bugfix.tacz;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.tacz.guns.entity.EntityKineticBullet;
 import mod.chloeprime.gunsmithlib.Config;
-import net.minecraft.network.protocol.game.ClientboundTeleportEntityPacket;
+import net.minecraft.network.protocol.game.ClientboundMoveEntityPacket;
 import net.minecraft.server.level.ServerEntity;
 import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Final;
@@ -11,22 +12,24 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 
+import java.util.function.Consumer;
+
 @Mixin(ServerEntity.class)
 public class TracerStartPosFixDisableBulletTrackingMixin {
     @Shadow @Final private Entity entity;
     @Shadow @Final private boolean trackDelta;
 
-    @ModifyExpressionValue(
+    @WrapOperation(
             method = "sendChanges",
-            at = @At(value = "NEW", target = "Lnet/minecraft/network/protocol/game/ClientboundTeleportEntityPacket;"))
-    private ClientboundTeleportEntityPacket doNotTeleportBullets(ClientboundTeleportEntityPacket original) {
-        if (!Config.IMPROVE_TRACER_ROTATION_STABILITY.get()) {
-            return original;
+            at = @At(value = "INVOKE", target = "Ljava/util/function/Consumer;accept(Ljava/lang/Object;)V"))
+    private <T> void doNotTeleportBullets(Consumer<T> broadcast, T packet, Operation<Void> original) {
+        if (Config.IMPROVE_TRACER_ROTATION_STABILITY.get()) {
+            if (!trackDelta && entity instanceof EntityKineticBullet) {
+                if (packet instanceof ClientboundMoveEntityPacket) {
+                    return;
+                }
+            }
         }
-        if (entity instanceof EntityKineticBullet) {
-            return trackDelta ? original : null;
-        } else {
-            return original;
-        }
+        original.call(broadcast, packet);
     }
 }
