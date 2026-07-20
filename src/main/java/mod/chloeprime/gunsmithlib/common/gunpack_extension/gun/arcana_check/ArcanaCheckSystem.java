51eac744579fa2cf7afe2a6ab0c6ed292c9e73ef
Chloe Prime <chloe_von_einzbern@qq.com>
2026-07-21 03:00:00 +0800

    再次修复曳光弹倾斜的 bug，修复 arcana 巡飞弹失速的 bug

--- src/main/java/mod/chloeprime/gunsmithlib/common/gunpack_extension/gun/arcana_check/ArcanaCheckSystem.java
+++ src/main/java/mod/chloeprime/gunsmithlib/common/gunpack_extension/gun/arcana_check/ArcanaCheckSystem.java
@@ -2,11 +2,10 @@ package mod.chloeprime.gunsmithlib.common.gunpack_extension.gun.arcana_check;
 
 import mod.chloeprime.gunsmithlib.api.util.GunInfo;
 import mod.chloeprime.gunsmithlib.common.gunpack_extension.gun.EnhancedGunData;
-import net.minecraftforge.fml.ModList;
+import mod.chloeprime.gunsmithlib.compat.ModInstallationStatus;
 
 public class ArcanaCheckSystem {
-    public static final String ARCANA_MOD_ID = "taczexpands";
-    public static final boolean ARCANA_INSTALLED = ModList.get().isLoaded(ARCANA_MOD_ID);
+    public static final boolean ARCANA_INSTALLED = ModInstallationStatus.ARCANA_INSTALLED;
 
     public static boolean shouldHintArcanaInstallation(GunInfo gun) {
         if (ARCANA_INSTALLED) {
