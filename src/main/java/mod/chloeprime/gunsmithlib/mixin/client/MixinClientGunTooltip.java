b7d87dd2559faba83b3a9ff04e34e8a6cb3bb98d
Chloe Prime <chloe_von_einzbern@qq.com>
2026-07-30 17:24:31 +0800

    破除 Gun Tooltip 行数限制，按住 Shift 以展开
    (cherry picked from commit 44dd927f15a7543d7074d96be7449e496d31cb01)

--- src/main/java/mod/chloeprime/gunsmithlib/mixin/client/MixinClientGunTooltip.java
+++ src/main/java/mod/chloeprime/gunsmithlib/mixin/client/MixinClientGunTooltip.java
@@ -16,11 +16,13 @@ import mod.chloeprime.gunsmithlib.api.client.scripting_v2.GunDisplayProperties;
 import mod.chloeprime.gunsmithlib.api.common.GunAttributes;
 import mod.chloeprime.gunsmithlib.api.util.GunInfo;
 import mod.chloeprime.gunsmithlib.client.ClientInternalEvents;
+import mod.chloeprime.gunsmithlib.client.gui.ExpandableGunTooltip;
 import mod.chloeprime.gunsmithlib.client.impl.scripting_v2.hooks.ModifyDisplayPropertyHook;
 import mod.chloeprime.gunsmithlib.common.util.GsHelper;
 import mod.chloeprime.gunsmithlib.common.util.GsHelper1211;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.util.FormattedCharSequence;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.neoforge.common.NeoForge;
 import org.jetbrains.annotations.Nullable;
@@ -36,6 +38,7 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.List;
 import java.util.function.BiFunction;
 
 @Mixin(value = ClientGunTooltip.class, remap = false)
@@ -234,6 +237,17 @@ public abstract class MixinClientGunTooltip {
             at = @At(value = "INVOKE", target = "Lcom/tacz/guns/api/item/IGun;useDummyAmmo(Lnet/minecraft/world/item/ItemStack;)Z"))
     private int modifyDisplayedAmmoCount(int original) {
         return ModifyDisplayPropertyHook.modifyProperty(gun, GunDisplayProperties.AMMO_AMOUNT, Integer.class, original);
+    }
+
+    // 破除 Tooltip 行数限制
+
+    @WrapOperation(
+            method = "getText",
+            at = @At(value = "INVOKE", target = "Ljava/util/List;subList(II)Ljava/util/List;"))
+    private
+    List<FormattedCharSequence>
+    breakTooltipLineLimit(List<FormattedCharSequence> raw, int start, int end, Operation<List<FormattedCharSequence>> original) {
+        return ExpandableGunTooltip.expand(raw, original.call(raw, start, end));
     }
 
     @Unique
